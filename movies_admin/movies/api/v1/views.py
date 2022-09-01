5804a476cc2ba587dba492228e34705f1d9cc780
Яблочкин Андрей Валерьевич <AVYablochkin@sberbank.ru>
2022-09-01 19:00:25 +0300

    - fix views.py pass all tests

--- movies_admin/movies/api/v1/views.py
+++ movies_admin/movies/api/v1/views.py
@@ -1,15 +1,30 @@
-from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Q
 from django.http import JsonResponse
 from django.views.generic.list import BaseListView
 from django.core.paginator import Paginator
+from django.contrib.postgres.aggregates import ArrayAgg
 from movies.models import FilmWork
 
 
 class MoviesApiMixin:
     def get_queryset(self):
-        query = FilmWork.objects.all().values("id", "title", "description", "creation_date")
+        query = FilmWork.objects.annotate(genres=ArrayAgg("genre__name", distinct=True),
+                                          actors=ArrayAgg("persons__full_name",
+                                                          distinct=True,
+                                                          filter=Q(personfilmwork__role="actor")),
+                                          directors=ArrayAgg("persons__full_name",
+                                                             distinct=True,
+                                                             filter=Q(personfilmwork__role="director")),
+                                          writers=ArrayAgg("persons__full_name",
+                                                             distinct=True,
+                                                             filter=Q(personfilmwork__role="writer")
+                                          )).values()
+        # query = FilmWork.objects.annotate(genres=ArrayAgg("genre__name", distinct=True), actors=ArrayAgg("persons__full_name", distinct=True), roles=ArrayAgg("person_film_work__role", distinct=True))
         return query  # Сформированный QuerySet
+
+    # def get_queryset(self):
+    #     query = FilmWork.objects.all().values("id", "title", "description", "creation_date")
+    #     return query  # Сформированный QuerySet
 
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context)
@@ -18,36 +33,37 @@ class MoviesApiMixin:
 class MoviesListApi(MoviesApiMixin, BaseListView):
     model = FilmWork
     http_method_names = ['get']  # Список методов, которые реализует обработчик
-    paginate_by = 5
+    paginate_by = 50
 
-
-    def get_queryset(self):
-        query = FilmWork.objects.all().values("id", "title", "description", "creation_date")
-        return query  # Сформированный QuerySet
+    # def get_queryset(self):
+    #     query = FilmWork.objects.all().select_related("genre", "person").values()
+    #     return query  # Сформированный QuerySet
 
     # def paginate_queryset(self, queryset, paginate_by):
     #     p = Paginator(queryset, paginate_by)
     #     return p, p.num_pages, queryset
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        page = self.kwargs.get('page', 1)
+        page = self.request.GET.get('page', 1)
         queryset = self.get_queryset()
         paginator = Paginator(queryset, self.paginate_by)
+        if page == "last":
+            page = paginator.num_pages
         result = list(paginator.page(page).object_list)
 
         if paginator.page(page).has_next():
             next_page = paginator.page(page).next_page_number()
         else:
-            next_page = page
+            next_page = None
 
         if paginator.page(page).has_previous():
             prev_page = paginator.page(page).previous_page_number()
         else:
-            prev_page = page
+            prev_page = None
 
         context = {
             'results': result,
-            'count': len(result),
+            'count': len(queryset),
             'prev': prev_page,
             'next': next_page,
             'total_pages': paginator.num_pages
@@ -63,6 +79,5 @@ class MoviesDetailApi(MoviesApiMixin, BaseListView):
     def get_context_data(self, **kwargs):
         uuid = self.kwargs.get('pk', None)  # получаем аргумент из ссылки
         data = self.get_queryset().get(id=uuid)
-        context = {"results": data}
-        return context
+        return data
 
